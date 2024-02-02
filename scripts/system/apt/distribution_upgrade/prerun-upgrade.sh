c6573b8d2a4a8b6b8a5c505e89d7ec6d82145d4e
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-02-02 19:36:24 +0100

    Update prerun-upgrade.sh

--- scripts/system/apt/distribution_upgrade/prerun-upgrade.sh
+++ scripts/system/apt/distribution_upgrade/prerun-upgrade.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Check if the script is run as root
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit 1
-fi
-
-# Backup the original sources.list file
-backup_file="/etc/apt/sources.list.bak"
-cp /etc/apt/sources.list "$backup_file"
-echo "Backup of sources.list created at $backup_file"
-
-# Function to determine the next Ubuntu version
-get_next_ubuntu_version() {
+# Function to determine the next Debian or Ubuntu version
+get_next_version() {
     current_version="$1"
 
     case $current_version in
@@ -21,6 +10,10 @@ get_next_ubuntu_version() {
         bionic) echo "focal" ;;
         focal) echo "hirsute" ;;
         hirsute) echo "impish" ;;
+        jessie) echo "stretch" ;;
+        stretch) echo "buster" ;;
+        buster) echo "bullseye" ;;
+        bullseye) echo "bookworm" ;;
         *) echo "Unknown version"; exit 1 ;;
     esac
 }
@@ -28,21 +21,51 @@ get_next_ubuntu_version() {
 # Function to update sources.list for distribution upgrade
 update_sources_list() {
     current_version="$1"
-    next_version="$(get_next_ubuntu_version $current_version)"
+    next_version="$(get_next_version $current_version)"
 
     sed -i "s/$current_version/$next_version/g" /etc/apt/sources.list
     echo "Sources.list updated for distribution upgrade from $current_version to $next_version."
 }
 
-# Check the current Ubuntu version
-current_version=$(lsb_release -sc)
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root."
+    exit 1
+fi
 
-case $current_version in
-    trusty|xenial|bionic|focal|hirsute)
-        update_sources_list "$current_version"
+# Determine distribution and version
+if [ -f /etc/debian_version ]; then
+    DISTRIBUTION="Debian"
+    VERSION=$(cat /etc/debian_version)
+elif [ -f /etc/lsb-release ]; then
+    DISTRIBUTION=$(grep "DISTRIB_ID" /etc/lsb-release | cut -d= -f2)
+    VERSION=$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d= -f2)
+else
+    echo "Unsupported distribution. Exiting."
+    exit 1
+fi
+
+# Ask the user to confirm
+read -p "This system seems to be a $DISTRIBUTION $VERSION system. Confirm (Y/N): " CONFIRM
+
+# Check the user's response
+if [ "$CONFIRM" != "Y" ] && [ "$CONFIRM" != "y" ]; then
+    echo "Update aborted by user."
+    exit 0
+fi
+
+# Backup the original sources.list file
+backup_file="/etc/apt/sources.list.bak"
+cp /etc/apt/sources.list "$backup_file"
+echo "Backup of sources.list created at $backup_file"
+
+# Check the current version and update sources.list for distribution upgrade
+case $VERSION in
+    trusty|xenial|bionic|focal|hirsute|jessie|stretch|buster|bullseye)
+        update_sources_list "$VERSION"
         ;;
     *)
-        echo "Unsupported Ubuntu version: $current_version."
+        echo "Unsupported version: $VERSION."
         exit 1
         ;;
 esac
