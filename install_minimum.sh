19a6c62871675ff85f2a5bfc621a26b3f1b202aa
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-02-02 18:04:37 +0100

    Update install_minimum.sh
    Removed some bugs (hopefully) and listened to "angry" feedback about not being able to say no to keep repo up to date.... The users are always (sometimes ;) right

--- install_minimum.sh
+++ install_minimum.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Switch to root user
+su -
+
+# Ask for the non-root username
+read -p "Enter the non-root username (usually your normal login name): " USERNAME
+
+# Use the provided username to determine the user's home directory
+USER_HOME=$(getent passwd "$USERNAME" | cut -d: -f6)
+
+# Ask if the user wants to create a subfolder
+read -p "Do you want to create a subfolder in $USER_HOME? (yes/no): " CREATE_SUBFOLDER
+
+# Set the repository directory based on user input
+if [ "$CREATE_SUBFOLDER" = "yes" ]; then
+    read -p "Enter subfolder name: " SUBFOLDER_NAME
+    REPO_DIR="$USER_HOME/$SUBFOLDER_NAME"
+else
+    REPO_DIR="$USER_HOME"
+fi
+
+# Ask if the user wants to keep the folder up to date
+read -p "Do you want to keep the folder up to date? (yes/no): " UPDATE_REPO
 
 # Update package lists
 apt update
@@ -30,17 +52,19 @@ ufw enable
 # Install fail2ban
 apt install -y fail2ban
 
-# Clone and update the repository once a week
-REPO_DIR="./debian_ubuntu_simplify"
-
-if [ ! -d "$REPO_DIR" ]; then
+# Clone or copy the repository based on user input
+if [ "$UPDATE_REPO" = "yes" ]; then
     git clone https://github.com/iceexplorer/debian_ubuntu_simplify "$REPO_DIR"
+    # Add a weekly cron job to update the repository
+    echo "0 0 * * 0 cd $REPO_DIR && git pull origin master" | crontab -
+else
+    wget https://github.com/iceexplorer/debian_ubuntu_simplify/archive/master.zip -O "$REPO_DIR/master.zip"
+    unzip "$REPO_DIR/master.zip" -d "$REPO_DIR"
+    rm "$REPO_DIR/master.zip"
 fi
-
-# Add a weekly cron job to update the repository
-echo "0 0 * * 0 cd $REPO_DIR && git pull origin master" | crontab -
 
 # Provide information to the user
 echo "Essential tools, dependencies, SSH server, SSL Engine, UFW, and fail2ban have been installed and configured."
 echo "UFW has been configured to allow traffic on ports 22, 80, and 443."
-echo "The repository https://github.com/iceexplorer/debian_ubuntu_simplify has been cloned into $REPO_DIR and will be updated once a week."
+echo "The repository has been cloned or copied to $REPO_DIR."
+echo "User $USERNAME has been used for the installation."
