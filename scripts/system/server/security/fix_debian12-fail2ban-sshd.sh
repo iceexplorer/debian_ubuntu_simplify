0f51f7b0e1e744b9fed7e2cff621f887b7d6b627
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-02-21 23:01:36 +0100

    Update fix_debian12-fail2ban-sshd.sh
    Feedback told me that that it also was needed to have python3-systemd installed., so I added it just in case it is true ;)

--- scripts/system/server/security/fix_debian12-fail2ban-sshd.sh
+++ scripts/system/server/security/fix_debian12-fail2ban-sshd.sh
@@ -2,6 +2,11 @@
 
 # This script should fix the problems to start Fail2Ban on Debian 12. DO NOT use this script on other Debian systems! From Debian 12, maintainers have changed where the needed logs are. Therefore Fail2an fails
 
+# This command installs the 'python3-systemd' package, which someone commented that is required for systemd backend support in Fail2Ban. I always have this installed, so I can`t verify if it is true or not, so I just added it. No big deal
+
+sudo apt-get update
+sudo apt-get install -y python3-systemd
+
 # Enable Fail2Ban for SSH
 # [sshd] section is created in the jail.local file with 'enabled=true'
 echo -e "[sshd]\nenabled=true" | sudo tee /etc/fail2ban/jail.local
