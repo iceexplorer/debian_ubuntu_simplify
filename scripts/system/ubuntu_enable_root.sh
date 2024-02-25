ab0e9853f947c3bfe45a3203b1314d3975583acb
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-02-25 18:15:51 +0100

    Update ubuntu_enable_root.sh

--- scripts/system/ubuntu_enable_root.sh
+++ scripts/system/ubuntu_enable_root.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# Prompt the user for their password
-echo -n "Enter your user password (sudo password): "
-read -s user_password
-echo
-
-# Check if the user has sudo privileges
-if ! echo "$user_password" | sudo -S true; then
-    echo "User does not have sudo privileges. Exiting."
-    exit 1
-fi
-
-# Prompt the user to enter a new password for the root user
-read -s -p "Enter the new password for the root user: " new_password
-echo
-
-# Check if the password is empty
-if [ -z "$new_password" ]; then
-    echo "Password cannot be empty. Exiting."
-    exit 1
-fi
-
-# Change the root password using sudo
-echo "root:$new_password" | sudo -S chpasswd
+sudo passwd root
 
 echo "Root password changed successfully."
